5e1bd263ff584b0276105b785dff8108411edc7e
Ryan-Mosier <116940632+Ryan-Mosier@users.noreply.github.com>
2023-02-23 13:14:16 -0700

    COLORS!!

---
+++ Level/colors.h
@@ -0,0 +1,77 @@
+//
+// Created by rymo on 11/23/22.
+//
+
+#ifndef RESET
+#define RESET   "\033[0m"
+#endif
+
+#ifndef BRIGHTBLUE
+#define BRIGHTBLUE "\u001b[34;1m"
+#endif
+
+#ifndef BLACK
+#define BLACK   "\033[30m"      /* Black */
+#endif
+
+#ifndef RED
+#define RED     "\033[31m"      /* Red */
+#endif
+
+#ifndef GREEN
+#define GREEN   "\033[32m"      /* Green */
+#endif
+
+#ifndef YELLOW
+#define YELLOW  "\033[33m"      /* Yellow */
+#endif
+
+#ifndef BLUE
+#define BLUE    "\033[34m"      /* Blue */
+#endif
+
+#ifndef MAGENTA
+#define MAGENTA "\033[35m"      /* Magenta */
+#endif
+
+#ifndef CYAN
+#define CYAN    "\033[36m"      /* Cyan */
+#endif
+
+#ifndef WHITE
+#define WHITE   "\033[37m"      /* White */
+#endif
+
+
+
+#ifndef BOLDBLACK
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#endif
+
+#ifndef BOLDRED
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#endif
+
+#ifndef BOLDGREEN
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#endif
+
+#ifndef BOLDYELLOW
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#endif
+
+#ifndef BOLDBLUE
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#endif
+
+#ifndef BOLDMAGENTA
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#endif
+
+#ifndef BOLDCYAN
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#endif
+
+#ifndef BOLDWHITE
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+#endif
