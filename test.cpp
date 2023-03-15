e7547d9cec527830c045573bf0f52b45bdbddab0
ryanm <rymo1472@colorado.edu>
2023-03-15 12:59:42 -0600

    adjustments to .gitignore

--- test.cpp
+++ test.cpp
@@ -1,49 +1,17 @@
-//
-// Created by ryanm on 3/7/2023.
-//
-
-#include <string>
-#include <chrono>
 #include <iostream>
-#include <thread>
-#include <termios.h>
-#include <unistd.h>
+#include <ncurses.h>
 
-using namespace std;
+int main() {
+    initscr(); // initialize ncurses
+    cbreak(); // set keyboard input to immediate mode
+    noecho(); // don't echo input characters to the screen
 
-int main(){
-    while(true){
-        string input;
-
-        /*#region Input*/
-        struct termios orig_termios;  // store original terminal settings
-
-        tcgetattr(STDIN_FILENO, &orig_termios);  // get current terminal settings
-        struct termios new_termios = orig_termios;
-        new_termios.c_lflag &= ~(ICANON | ECHO);  // set non-canonical mode and turn off echoing
-        tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);  // apply new terminal settings
-
-        auto start = std::chrono::steady_clock::now();  // get the start time
-
-        while (std::chrono::duration_cast<std::chrono::milliseconds>(
-                std::chrono::steady_clock::now() - start).count() <
-               1000)  // loop for 1 second
-        {
-            char c;
-            if (read(STDIN_FILENO, &c, 1) == 1)  // read one character of input
-            {
-                input += input;
-            }
-        }
-
-        tcsetattr(STDIN_FILENO, TCSANOW, &orig_termios);  // restore original terminal settings
-
-        /*#endregion*/
-
-        cout << "Your input was: " << input;
-        if(input == "e"){
-            return 1;
-        }
+    if(!has_colors()){
+        std::cout << "No colors :(\n";
+    }else{
+        std::cout << "Colors!\n";
     }
 
+    endwin(); // end ncurses mode
+    return 0;
 }
