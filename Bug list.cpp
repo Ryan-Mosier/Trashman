1b0aee7ca4f36d6dffd988b08d4403b79cb8f587
Ryan-Mosier <ryan.m.mosier@gmail.com>
2023-03-19 21:11:33 -0600

    lists

---
+++ Bug list.cpp
@@ -0,0 +1,6 @@
+//
+// Created by ryanm on 3/19/2023.
+//
+
+//TODO: One ghost never leaves the spawn room
+//TODO: ghosts eat each other ?!?!??
---
+++ WIP features.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ryanm on 3/19/2023.
+//
+
+//TODO: rendering
+
+
+//TODO: pellets
+
+//TODO: bigger map
+
+//TODO: ghosts respawn after death
